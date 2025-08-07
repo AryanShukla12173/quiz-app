fffd6da331785fc08957b286fb10fc5f1a1c058e
Aryan Shukla <6aryanshukla@gmail.com>
2025-08-07 12:45:33 +0530

    start from scratch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
-// app/layout.tsx
-import { AuthProvider } from '@/context/AuthContext';
-import '@/app/globals.css'; // Tailwind styles, etc.
+import '@/app/globals.css'; 
 
 export const metadata = {
   title: 'Quiz App',
-  description: 'A platform for generating and ',
+  description: 'A platform for generating and using code test and administring it to users ',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-background text-foreground min-h-screen">
-        <AuthProvider>
-          {children} {/* This renders your dashboard or other pages */}
-        </AuthProvider>
+        {children}
       </body>
     </html>
   );
