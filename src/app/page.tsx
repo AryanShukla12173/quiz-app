98c9bbf86e7ad1542f7b337f133a973fabfb4d72
Aryan Shukla <6aryanshukla@gmail.com>
2025-08-15 17:52:46 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,7 @@ import { createClient } from "@/lib/utils/supabase/server";
 import "dotenv/config";
 import { redirect } from "next/navigation";
 export default async function Home() {
-  const supabase = await createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  const supabase = await createClient();
   const res = await supabase.auth.getUser();
   if (!res.data.user?.is_anonymous) {
     redirect("/test-admin-dashboard");
